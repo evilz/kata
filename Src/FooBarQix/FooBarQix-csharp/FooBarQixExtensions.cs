26b3f0f851db6a2fd0f4d1a7b32eb02f58949dd0
evilz <evilznet@gmail.com>
2015-05-22 09:17:47 +0200

    Refactoring FooBarQix in linq using Dicotionary

--- Src/FooBarQix/FooBarQix-csharp/FooBarQixExtensions.cs
+++ Src/FooBarQix/FooBarQix-csharp/FooBarQixExtensions.cs
@@ -1,47 +1,28 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace FooBarQix_csharp
 {
     public static class FooBarQixExtensions
     {
+
+        private static readonly Dictionary<int, string> _fooBarQixMap = new Dictionary<int, string>
+            {
+                {3,"foo" },
+                {5,"bar" },
+                {7,"qix" }
+            };
+
         public static string FooBarQix(this int number)
         {
-            string result = String.Empty;
+            var result = _fooBarQixMap.Keys
+                            .Where(i => number%i == 0)
+                            .Aggregate(string.Empty, (current, i) => current + _fooBarQixMap[i]);
 
-            string s = number.ToString();
-            Func<int, int, bool> isMod = (n, d) => n%d == 0;
+            result = number.ToString()
+                        .Where(c => _fooBarQixMap.ContainsKey(c-'0'))
+                        .Aggregate(result, (current, c) => current + _fooBarQixMap[c - '0']);
 
-            if (isMod(number, 3))
-            {
-                result += "foo";
-            }
-            if (isMod(number, 5))
-            {
-                result += "bar";
-            }
-            if (isMod(number, 7))
-            {
-                result += "qix";
-            }
-
-            foreach (var c in s)
-            {
-                if (c == '3')
-                {
-                    result += "foo";
-                }
-
-                if (c == '5')
-                {
-                    result += "bar";
-                }
-
-                if (c == '7')
-                {
-                    result += "qix";
-                }
-            }
-            
 
             return string.IsNullOrEmpty(result) ? number.ToString() : result;
         }
