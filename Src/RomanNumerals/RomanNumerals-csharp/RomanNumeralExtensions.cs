03f441a74b51fbfb0c95a98f7db5482e34c2399a
evilz <evilznet@gmail.com>
2015-05-26 19:59:32 +0200

    Init Roman Numerals kata

---
+++ Src/RomanNumerals/RomanNumerals-csharp/RomanNumeralExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals_csharp
+{
+    public static class RomanNumeralExtensions
+    {
+        public static string ToRomanNumeral(this int number)
+        {
+            return null;
+        }
+    }
+}
---
+++ Src/RomanNumerals/RomanNumerals-csharp/RomanNumeralsTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace RomanNumerals_csharp
+{
+
+    public class RomanNumeralsTests
+    {
+        [TestCase(1,"I")]
+
+        public void ToRomanNumeral_Should_Return_SimpleLetter(int number, string expected)
+        {
+            var result = number.ToRomanNumeral();
+            Assert.AreEqual(expected,result);
+        }
+    }
+}
