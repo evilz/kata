b50d31f3a180ca07ec7618c7c41ac229e2a310fe
evilz <evilznet@gmail.com>
2015-05-27 08:28:11 +0200

    Roman Numeral Before refactoring

--- Src/RomanNumerals/RomanNumerals-csharp/RomanNumeralExtensions.cs
+++ Src/RomanNumerals/RomanNumerals-csharp/RomanNumeralExtensions.cs
@@ -1,16 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace RomanNumerals_csharp
 {
+
     public static class RomanNumeralExtensions
     {
+        private static Dictionary<int, string> _map = new Dictionary<int, string>
+        {
+            {1, "I"},
+            {5, "V"},
+            {10, "X"},
+            {50, "L"},
+            {100, "C"},
+            {500, "D"},
+            {1000, "M"}
+        };
+
         public static string ToRomanNumeral(this int number)
         {
-            return null;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1000; i > 0; i = i / 10)
+            {
+                int part = number / i;
+                if(part == 0) continue;
+                else if (part == 1) sb.Append(_map[i]);
+                else if(part == 2) sb.Append(_map[i] + _map[i]);
+                else if(part == 3) sb.Append(_map[i] + _map[i] + _map[i]);
+                else if(part == 4) sb.Append(_map[i] + _map[i * 5]);
+                else if(part == 5) sb.Append(_map[i * 5]);
+                else if(part == 6) sb.Append(_map[i * 5] + _map[i]);
+                else if(part == 7) sb.Append(_map[i * 5] + _map[i] + _map[i]);
+                else if(part == 8) sb.Append(_map[i * 5] + _map[i] + _map[i] + _map[i]);
+                else if(part == 9) sb.Append(_map[i] + _map[i * 10]);
+
+                number -= part * i;
+            }
+            return sb.ToString();
         }
     }
 }
--- Src/RomanNumerals/RomanNumerals-csharp/RomanNumeralsTests.cs
+++ Src/RomanNumerals/RomanNumerals-csharp/RomanNumeralsTests.cs
@@ -6,11 +6,61 @@ namespace RomanNumerals_csharp
     public class RomanNumeralsTests
     {
         [TestCase(1,"I")]
+        [TestCase(5,"V")]
+        [TestCase(10,"X")]
+        [TestCase(50,"L")]
+        [TestCase(100,"C")]
+        [TestCase(500,"D")]
+        [TestCase(1000,"M")]
 
         public void ToRomanNumeral_Should_Return_SimpleLetter(int number, string expected)
         {
             var result = number.ToRomanNumeral();
             Assert.AreEqual(expected,result);
+        }
+
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(20, "XX")]
+        [TestCase(30, "XXX")]
+        [TestCase(200, "CC")]
+        [TestCase(300, "CCC")]
+        [TestCase(2000, "MM")]
+        [TestCase(3000, "MMM")]
+
+        public void ToRomanNumeral_Should_RepeatUnitLetter(int number, string expected)
+        {
+            var result = number.ToRomanNumeral();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(60, "LX")]
+        [TestCase(700, "DCC")]
+        public void ToRomanNumeral_Should_AddAndRepeatUnitLetterAfterMidVal(int number, string expected)
+        {
+            var result = number.ToRomanNumeral();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(9, "IX")]
+        [TestCase(90, "XC")]
+        [TestCase(900, "CM")]
+        public void ToRomanNumeral_Should_SubstractOneSmallValueFromLager(int number, string expected)
+        {
+            var result = number.ToRomanNumeral();
+            Assert.AreEqual(expected, result);
+        }
+
+
+
+        [TestCase(1903, "MCMIII")]
+        public void ToRomanNumeral_Should_BreakNumberInDigit(int number, string expected)
+        {
+            var result = number.ToRomanNumeral();
+            Assert.AreEqual(expected, result);
         }
     }
 }
