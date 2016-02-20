d2f0424dca1a911597044caaa08f64c856598a0b
Vincent B <evilznet@gmail.com>
2016-02-20 15:59:39 +0100

    Update dir structure

---
+++ Src/TDD-Kata/Minesweeper/Minesweeper-csharp/MinesweeperTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Minesweeper_csharp
+{
+    public class MinesweeperTests
+    {
+        [TestCase("3 5\r\n.....\r\n.....\r\n.....\r\n0 0", 3, 5)]
+        [TestCase("2 1\r\n.\r\n.\r\n0 0", 2, 1)]
+        public void Should_Parse_firstline_as_fieldsize(string input, int height, int width)
+        {
+
+            var minesweeper = Minesweeper.Parse(input.ToReader());
+
+            var firstField = minesweeper.Fields.First();
+
+            Assert.AreEqual(height, firstField.Height);
+            Assert.AreEqual(width, firstField.Width);
+        }
+
+        [Test]
+        public void Should_Parse_star_char_as_mine_cell()
+        {
+            var input = "1 1\r\n*0 0";
+
+            TextReader reader = new StringReader(input);
+            var minesweeper = Minesweeper.Parse(reader);
+
+            var firstField = minesweeper.Fields.First();
+
+            Assert.That(firstField.GetSolution(), Does.Contain("*"));
+        }
+
+        [Test]
+        public void Should_Parse_dot_char_as_clear_cell()
+        {
+            var input = "1 1\r\n.0 0";
+
+            TextReader reader = new StringReader(input);
+            var minesweeper = Minesweeper.Parse(reader);
+
+            var firstField = minesweeper.Fields.First();
+
+            Assert.That(firstField.GetSolution(), Does.Contain("0"));
+        }
+
+        [TestCase("3 3\r\n...\r\n.*.\r\n...0 0", "111\r\n1*1\r\n111")]
+        [TestCase("3 3\r\n***\r\n*.*\r\n***0 0", "***\r\n*8*\r\n***")]
+        [TestCase("3 3\r\n*..\r\n...\r\n...0 0", "*10\r\n110\r\n000")]
+        public void Should_Count_Neighbours_mine_cells(string input, string excpectedInOutput)
+        {
+            TextReader reader = new StringReader(input);
+            var minesweeper = Minesweeper.Parse(reader);
+
+            var firstField = minesweeper.Fields.First();
+
+            Assert.That(firstField.GetSolution(), Does.Contain(excpectedInOutput));
+        }
+
+        [Test]
+        public void Should_Parse_Expected_number_of_fields()
+        {
+            var input = @"4 4
+*...
+....
+.*..
+....
+3 5
+**...
+.....
+.*...
+0 0";
+            var minesweeper = Minesweeper.Parse(input.ToReader());
+
+
+            Assert.AreEqual(2, minesweeper.Fields.Count());
+        }
+
+        [Test]
+        public void Should_Return_fieldNumber_has_header_of_each_solution()
+        {
+            var input = @"4 4
+*...
+....
+.*..
+....
+3 5
+**...
+.....
+.*...
+0 0";
+            var minesweeper = Minesweeper.Parse(input.ToReader());
+
+            Assert.That(minesweeper.GetSolution(), Does.StartWith("Field #1:" + Environment.NewLine));
+            Assert.That(minesweeper.GetSolution(), Does.Contain("Field #2:" + Environment.NewLine));
+
+        }
+
+    }
+
+    public static class Helpers
+    {
+        public static TextReader ToReader(this string input)
+        {
+            return new StringReader(input);
+        }
+    }
+}
--- paket-files/fsharp/FAKE/modules/Octokit/Octokit.fsx
+++ paket-files/fsharp/FAKE/modules/Octokit/Octokit.fsx
@@ -5,45 +5,62 @@ open Octokit
 open System
 open System.IO
 
-type Draft = 
+type Draft =
     { Client : GitHubClient
       Owner : string
       Project : string
       DraftRelease : Release }
 
-let rec private retry count asyncF = 
-    async { 
-        try 
-            return! asyncF
-        with _ when count > 0 -> return! retry (count - 1) asyncF
-    }
+let private isRunningOnMono = System.Type.GetType ("Mono.Runtime") <> null
 
+let rec private retry count asyncF =
+    // This retry logic causes an exception on Mono:
+    // https://github.com/fsharp/fsharp/issues/440
+    if isRunningOnMono then
+        asyncF
+    else
+        async {
+            try
+                return! asyncF
+            with _ when count > 0 -> return! retry (count - 1) asyncF
+        }
 
-let createClient user password = 
-    async { 
+let createClient user password =
+    async {
         let github = new GitHubClient(new ProductHeaderValue("FAKE"))
         github.Credentials <- Credentials(user, password)
         return github
     }
 
-let createDraft owner project version prerelease (notes: string seq) (client : Async<GitHubClient>) =     
-    async { 
+let createClientWithToken token =
+    async {
+        let github = new GitHubClient(new ProductHeaderValue("FAKE"))
+        github.Credentials <- Credentials(token)
+        return github
+    }
+
+let private makeRelease draft owner project version prerelease (notes:seq<string>) (client : Async<GitHubClient>) =
+    async {
         let data = new NewRelease(version)
         data.Name <- version
         data.Body <- String.Join(Environment.NewLine, notes)
-        data.Draft <- true
+        data.Draft <- draft
         data.Prerelease <- prerelease
         let! client' = client
         let! draft = Async.AwaitTask <| client'.Release.Create(owner, project, data)
-        printfn "Created draft release id %d" draft.Id
+        let draftWord = if data.Draft then " draft" else ""
+        printfn "Created%s release id %d" draftWord draft.Id
         return { Client = client'
                  Owner = owner
                  Project = project
                  DraftRelease = draft }
     } |> retry 5
 
-let uploadFile fileName (draft : Async<Draft>) = 
-    async { 
+let createDraft owner project version prerelease notes client = makeRelease true owner project version prerelease notes client
+let createRelease owner project version prerelease notes client = makeRelease false owner project version prerelease notes client
+
+let uploadFile fileName (draft : Async<Draft>) =
+    async {
         let fi = FileInfo(fileName)
         let archiveContents = File.OpenRead(fi.FullName)
         let assetUpload = new ReleaseAssetUpload(fi.Name,"application/octet-stream",archiveContents,Nullable<TimeSpan>())
@@ -53,8 +70,8 @@ let uploadFile fileName (draft : Async<Draft>) =
         return draft'
     } |> retry 5
 
-let releaseDraft (draft : Async<Draft>) = 
-    async { 
+let releaseDraft (draft : Async<Draft>) =
+    async {
         let! draft' = draft
         let update = draft'.DraftRelease.ToUpdate()
         update.Draft <- Nullable<bool>(false)
